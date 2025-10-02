81e1eb9f1ee7ebf3a2264666bc6877d7d44f6e50
Travis Zhang <wr.zhang25@gmail.com>
2025-10-02 21:06:30 +0800

    chore: daily development

--- packages/frontend/src/pages/projects/[id]/index.tsx
+++ packages/frontend/src/pages/projects/[id]/index.tsx
@@ -3,7 +3,11 @@ import {useParams} from "react-router-dom";
 const ProjectDetail = () => {
   const projectID= useParams()
   return <div>
+    <video src={'http://192.168.3.52:3000/public/app/f7979606eb56f96f0f2f1f792cf7f2dcc8fbce01eda89db0d0dd2488413ce1b5.mp4'}/>
 
+    <div>
+      右边
+    </div>
   </div>
 }
 
