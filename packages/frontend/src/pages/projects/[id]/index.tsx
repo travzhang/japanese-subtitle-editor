f4925404bd94cc5ebaa6b22896779406b444d96a
Travis Zhang <wr.zhang25@gmail.com>
2025-10-02 20:58:41 +0800

    chore: daily development

---
+++ packages/frontend/src/pages/projects/[id]/index.tsx
@@ -0,0 +1,11 @@
+import {useParams} from "react-router-dom";
+
+const ProjectDetail = () => {
+  const projectID= useParams()
+  console.log(projectID,'projectID')
+  return <div>
+    sss
+  </div>
+}
+
+export default ProjectDetail
